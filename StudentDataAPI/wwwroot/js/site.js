fae1e5048b6a76e59d02ad6f92910d0de5c94684
PrithviRajSalwan <86774313+PrithviRajSalwan@users.noreply.github.com>
2021-07-01 17:18:40 +0530

    Update site.js

--- StudentDataAPI/wwwroot/js/site.js
+++ StudentDataAPI/wwwroot/js/site.js
@@ -593,5 +593,13 @@ $(document).ready(function () {
         }
 
     });
+    
+    $('#StudentSearch').keypress(function (e) {
+ var key = e.which;
+ if(key == 13)  // the enter key code
+  {
+    $("#FilterDataSearch").trigger("click"); 
+  }
+});
 
 });
